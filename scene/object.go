11219476ed87a5b1de4ba551f9e92ccfe73d48b0
Michal Vala <michal.vala@gmail.com>
2019-02-08 18:41:20 +0100

    added basic collisions and 2 move methods to object

--- scene/object.go
+++ scene/object.go
@@ -6,6 +6,9 @@ type Object interface {
 	GetXsize() int
 	GetYsize() int
 	GetPixels() []Pixel
+
+	MoveBy(x float64, y float64)
+	MoveTo(x float64, y float64)
 }
 
 type Pixel struct {
@@ -16,4 +19,14 @@ type Pixel struct {
 
 func NewPixel(x int, y int, color [4]byte) Pixel {
 	return Pixel{X: x, Y: y, Color: color}
+}
+
+func Collides(o1 Object, o2 Object) bool {
+	xColision := o1.GetXoffset() + o1.GetXsize() > o2.GetXoffset() &&
+		o1.GetXoffset() < o2.GetXoffset() + o2.GetXsize()
+
+	yColision := o1.GetYoffset() + o1.GetYsize() > o2.GetYoffset() &&
+		o1.GetYoffset() < o2.GetYoffset() + o2.GetYsize()
+
+	return xColision && yColision
 }
