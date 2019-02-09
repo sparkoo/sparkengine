37f3d3047d1fe74967665b4980100e601500df91
Michal Vala <michal.vala@gmail.com>
2019-02-09 06:10:50 +0100

    generic object base

--- scene/object.go
+++ scene/object.go
@@ -6,9 +6,51 @@ type Object interface {
 	GetXsize() int
 	GetYsize() int
 	GetPixels() []Pixel
+	GetPos() (float64, float64)
 
 	MoveBy(x float64, y float64)
 	MoveTo(x float64, y float64)
+}
+
+type Base struct {
+	xpos  float64
+	ypos  float64
+	xsize int
+	ysize int
+}
+
+func NewBase(xpos float64, ypos float64, xsize int, ysize int) *Base {
+	return &Base{xpos: xpos, ypos: ypos, xsize: xsize, ysize: ysize}
+}
+
+func (o *Base) GetXoffset() int {
+	return int(o.xpos)
+}
+
+func (o *Base) GetYoffset() int {
+	return int(o.ypos)
+}
+
+func (o *Base) GetPos() (float64, float64) {
+	return o.xpos, o.ypos
+}
+
+func (o *Base) GetXsize() int {
+	return o.xsize
+}
+
+func (o *Base) GetYsize() int {
+	return o.ysize
+}
+
+func (o *Base) MoveBy(x float64, y float64) {
+	o.xpos += x
+	o.ypos += y
+}
+
+func (o *Base) MoveTo(x float64, y float64) {
+	o.xpos = x
+	o.ypos = y
 }
 
 type Pixel struct {
@@ -22,11 +64,11 @@ func NewPixel(x int, y int, color [4]byte) Pixel {
 }
 
 func Collides(o1 Object, o2 Object) bool {
-	xColision := o1.GetXoffset() + o1.GetXsize() > o2.GetXoffset() &&
-		o1.GetXoffset() < o2.GetXoffset() + o2.GetXsize()
+	xColision := o1.GetXoffset()+o1.GetXsize() > o2.GetXoffset() &&
+		o1.GetXoffset() < o2.GetXoffset()+o2.GetXsize()
 
-	yColision := o1.GetYoffset() + o1.GetYsize() > o2.GetYoffset() &&
-		o1.GetYoffset() < o2.GetYoffset() + o2.GetYsize()
+	yColision := o1.GetYoffset()+o1.GetYsize() > o2.GetYoffset() &&
+		o1.GetYoffset() < o2.GetYoffset()+o2.GetYsize()
 
 	return xColision && yColision
 }
