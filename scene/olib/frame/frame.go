0063359c2197d7d54ca7619d4a7be2cbdabf4449
Michal Vala <michal.vala@gmail.com>
2019-02-10 06:13:16 +0100

    rectangle to objects lib

---
+++ scene/olib/frame/frame.go
@@ -0,0 +1,47 @@
+package frame
+
+import "github.com/sparkoo/sparkengine/scene"
+
+type Frame struct {
+	*scene.Base
+	color  scene.Color
+	pixels []scene.Pixel
+}
+
+func (f *Frame) GetPixels() []scene.Pixel {
+	return f.pixels
+}
+
+func NewFrame(xpos float64, ypos float64, xsize int, ysize int, color scene.Color) *Frame {
+	return &Frame{
+		Base: scene.NewBase(xpos, ypos, xsize, ysize), color: color,
+		pixels: initPixels(xsize, ysize, color)}
+}
+
+func initPixels(xsize int, ysize int, color scene.Color) []scene.Pixel {
+	// top and bottom lines -> xsize * 2
+	// left and right lines -> ysize * 2
+	// 4 corners are in both top/bottom and left/right lines -> -4
+	pixels := make([]scene.Pixel, (xsize*2)+((ysize*2)-4))
+	pi := 0
+
+	// top and bottom lines
+	for xi := 0; xi < xsize; xi++ {
+		pixels[pi] = scene.NewPixel(xi, 0, color)
+		pi++
+
+		pixels[pi] = scene.NewPixel(xi, ysize, color)
+		pi++
+	}
+
+	// left and right lines
+	for yi := 1; yi < ysize-1; yi++ {
+		pixels[pi] = scene.NewPixel(0, yi, color)
+		pi++
+
+		pixels[pi] = scene.NewPixel(xsize, yi, color)
+		pi++
+	}
+
+	return pixels
+}
