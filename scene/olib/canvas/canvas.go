d07f703f7a8ce79cc2f833bbf56fbca02dc85111
Michal Vala <michal.vala@gmail.com>
2019-02-11 12:37:08 +0100

    canvas base

---
+++ scene/olib/canvas/canvas.go
@@ -0,0 +1,34 @@
+package canvas
+
+import "github.com/sparkoo/sparkengine/scene"
+
+type Canvas struct {
+	*scene.Base
+
+	backgroundColor scene.Color
+	canvasPixels    []scene.Pixel
+}
+
+func NewCanvas(x float64, y float64, xsize int, ysize int, backgroundColor scene.Color) *Canvas {
+	return &Canvas{
+		Base:            scene.NewBase(x, y, xsize, ysize),
+		backgroundColor: backgroundColor,
+		canvasPixels:    initPixels(backgroundColor, xsize, ysize),
+	}
+}
+
+func initPixels(color scene.Color, width int, height int) []scene.Pixel {
+	pixels := make([]scene.Pixel, width*height)
+	pi := 0
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			pixels[pi] = scene.NewPixel(x, y, color)
+			pi++
+		}
+	}
+	return pixels
+}
+
+func (c *Canvas) GetPixels() []scene.Pixel {
+	return c.canvasPixels
+}
