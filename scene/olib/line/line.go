071b8e61682cf68e5db4e709ab43d3eb427053e4
Michal Vala <michal.vala@gmail.com>
2019-02-11 05:56:05 +0100

    fix drawing lines, should work now

--- scene/olib/line/line.go
+++ scene/olib/line/line.go
@@ -2,16 +2,17 @@ package line
 
 import (
 	"github.com/sparkoo/sparkengine/scene"
-	"log"
 	"math"
 )
 
+// [x1;y1] determines position of the line
+// pixels can go to negative positions, thus lines are not positioned by top-left corner, but by p1
 type Line struct {
 	*scene.Base
-	p1x float64
-	p1y float64
-	p2x float64
-	p2y float64
+	x1 float64
+	y1 float64
+	x2 float64
+	y2 float64
 
 	color scene.Color
 
@@ -23,36 +24,19 @@ func (l *Line) GetPixels() []scene.Pixel {
 }
 
 func NewLine(x1 float64, y1 float64, x2 float64, y2 float64, color scene.Color) *Line {
-	x, y, xsize, ysize := coords(x1, y1, x2, y2)
+	xsize := int(math.Abs(x1 - x2))
+	ysize := int(math.Abs(y1 - y2))
 
 	return &Line{
-		Base:   scene.NewBase(x, y, xsize, ysize),
+		Base:   scene.NewBase(x1, y1, xsize, ysize),
+		x1:     x1,
+		y1:     y1,
+		x2:     x2,
+		y2:     y2,
+		color:  color,
 		pixels: initPixels(0, 0, x2-x1, y2-y1, color)}
 }
 
-func coords(x1 float64, y1 float64, x2 float64, y2 float64) (x float64, y float64, xsize int, ysize int) {
-	// determine most left point and X size
-	if x1 < x2 {
-		x = x1
-		xsize = int(x2 - x1)
-	} else {
-		x = x2
-		xsize = int(x1 - x2)
-	}
-
-	// determine most top point and y size
-	if y1 < y2 {
-		y = y1
-		ysize = int(y2 - y1)
-	} else {
-		y = y2
-		ysize = int(y1 - y2)
-	}
-	return
-}
-
-// TODO: this drawing works just for horizontal-ish lines. More vertical the line is, more broken it is.
-//  implement this https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
 func initPixels(x0 float64, y0 float64, x1 float64, y1 float64, color scene.Color) []scene.Pixel {
 	pixels := make([]scene.Pixel, 0)
 
@@ -70,27 +54,24 @@ func initPixels(x0 float64, y0 float64, x1 float64, y1 float64, color scene.Colo
 		}
 	}
 
-	log.Println(pixels)
-
 	return pixels
 }
 
-func plotLineLow(x0 float64, y0 float64, x1 float64, y1 float64, color scene.Color) []scene.Pixel {
+func plotLineLow(x1 float64, y1 float64, x2 float64, y2 float64, color scene.Color) []scene.Pixel {
 	var pixels []scene.Pixel
 
-	dx := x1 - x0
-	dy := y1 - y0
+	dx := x2 - x1
+	dy := y2 - y1
 	yi := 1.0
 	if dy < 0 {
 		yi = -1
 		dy = -dy
 	}
 	D := 2*dy - dx
-	y := y0
+	y := y1
 
-	for x := x0; x < x1; x++ {
+	for x := x1; x < x2; x++ {
 		pixels = append(pixels, scene.NewPixel(int(x), int(y), color))
-		log.Println(x, y)
 		if D > 0 {
 			y = y + yi
 			D = D - 2*dx
@@ -101,22 +82,21 @@ func plotLineLow(x0 float64, y0 float64, x1 float64, y1 float64, color scene.Col
 	return pixels
 }
 
-func plotLineHigh(x0 float64, y0 float64, x1 float64, y1 float64, color scene.Color) []scene.Pixel {
+func plotLineHigh(x1 float64, y1 float64, x2 float64, y2 float64, color scene.Color) []scene.Pixel {
 	var pixels []scene.Pixel
 
-	dx := x1 - x0
-	dy := y1 - y0
+	dx := x2 - x1
+	dy := y2 - y1
 	xi := 1.0
 	if dx < 0 {
 		xi = -1
 		dx = -dx
 	}
 	D := 2*dx - dy
-	x := x0
+	x := x1
 
-	for y := y0; y < y1; y++ {
+	for y := y1; y < y2; y++ {
 		pixels = append(pixels, scene.NewPixel(int(x), int(y), color))
-		log.Println(x, y)
 		if D > 0 {
 			x = x + xi
 			D = D - 2*dy
