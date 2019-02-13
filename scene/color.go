8ee5fb811c19a5d432e9161b08b0bb1a02a56ded
Michal Vala <michal.vala@gmail.com>
2019-02-13 09:36:30 +0100

    color RGBA constants

--- scene/color.go
+++ scene/color.go
@@ -4,6 +4,13 @@ import "math/rand"
 
 type Color [4]byte
 
+const (
+	COLOR_R = 0
+	COLOR_G = 1
+	COLOR_B = 2
+	COLOR_A = 3
+)
+
 func RandomColor(alpha byte) Color {
 	return Color{
 		byte(rand.Intn(256)),
