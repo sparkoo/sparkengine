c30826f8b79904f93f057357146f5632d9169ee1
Michal Vala <michal.vala@gmail.com>
2019-02-13 10:38:49 +0100

    few default configs

--- core/conf.go
+++ core/conf.go
@@ -10,3 +10,15 @@ type Conf struct {
 func NewConf(tps int, fps int, screenWidth int32, screenHeight int32) *Conf {
 	return &Conf{tps: tps, fps: fps, screenWidth: screenWidth, screenHeight: screenHeight}
 }
+
+func NewConf30Ticks(width int32, height int32) *Conf {
+	return NewConf(30, 60, width, height)
+}
+
+func NewConf30T320W() *Conf {
+	return NewConf(30, 60, 320, 240)
+}
+
+func NewConf30T640W() *Conf {
+	return NewConf(30, 60, 640, 480)
+}
